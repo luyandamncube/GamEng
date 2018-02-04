06f41823d2e861ce22c8c95e63771357b600f316
luyandamncube <594488873@mylife.unisa.ac.za>
2018-02-04 13:43:22 -0500

    Completed lesson01

---
+++ lesson01/hello_SDL.cpp
@@ -0,0 +1,32 @@
+/* Intial program to create a window with a white fill
+ * Window pops up for two seconds, or displays error
+ * SDL_Init lets you call on functions from the SDL lib
+ * Remember to change SRC for SDL header files
+ */
+#include <SDL.h>
+#include <stdio.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int	main(int argc, char **argv)
+{
+	SDL_Window *window = NULL;
+	SDL_Surface *screenSurface = NULL; 
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		printf("SDL could not initialise! SDL_Error: %s\n", SDL_GetError());
+	else
+		window = SDL_CreateWindow("SDL Tutorial",  SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == NULL)
+		printf("Window could not be created! SDL_Error: %s\n",SDL_GetError());
+	else
+	{
+		 screenSurface = SDL_GetWindowSurface( window ); 
+		 SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) ); 
+		 SDL_UpdateWindowSurface( window ); 
+		 SDL_Delay( 2000 );
+	}
+	SDL_DestroyWindow( window );
+	SDL_Quit(); 
+	return 0;
+}
